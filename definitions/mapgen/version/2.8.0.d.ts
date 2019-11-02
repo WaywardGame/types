ec21b9e0cf56edcb86500f7f75a2ac055ca5c4a8
Gary Wilber <Spacetech326@gmail.com>
2019-11-02 01:58:40 -0700

    Update for 2.8.0-beta.dev.20191102

--- definitions/mapgen/version/2.8.0.d.ts
+++ definitions/mapgen/version/2.8.0.d.ts
@@ -8,8 +8,16 @@
  * Wayward is a copyrighted and licensed work. Modification and/or distribution of any source files is prohibited. If you wish to modify the game in any way, please refer to the modding guide:
  * https://waywardgame.github.io/
  */
+import EventEmitter from "event/EventEmitter";
 import IMapGen from "mapgen/IMapGen";
-export default class MapGen280 implements IMapGen {
+export interface IMapGen280Events {
+    /**
+     * To be set for when you want the player's spawn to be easy (locked to the top half of the map)
+     * @returns True if the player spawn's should be easy
+     */
+    isEasySpawn(): boolean | undefined;
+}
+export default class MapGen280 extends EventEmitter.Host<IMapGen280Events> implements IMapGen {
     generateWorld(generateNewWorld: boolean): void;
     private setupTiles;
 }
