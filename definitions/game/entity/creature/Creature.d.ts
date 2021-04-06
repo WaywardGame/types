d8bf75191cb00a925b3c8c5de89a725f2cf13b41
Gary Wilber <spacetech326@gmail.com>
2021-04-06 02:34:00 -0700

    Update for 2.10.0-beta.dev.20210406.1

--- definitions/game/entity/creature/Creature.d.ts
+++ definitions/game/entity/creature/Creature.d.ts
@@ -70,7 +70,12 @@ export default class Creature extends Entity implements IUnserializedCallback, I
      * @returns Returns whether the creature can keep moving (in the case of creatures with >= 2 speed)
      */
     checkUnder(x?: number, y?: number): boolean;
-    canSwapWith(player: Player, source?: string): boolean;
+    /**
+     * Check if this creature can swap with the player in the event that the player is moving into them
+     * @param player Player object
+     * @param source Source string. Set to undefined if this is being called from the clientside
+     */
+    canSwapWith(player: Player, source: string | undefined): boolean;
     getOwner(): Player | undefined;
     damage(damageInfo: IDamageInfo): number | undefined;
     damage(damageInfo: IDamageInfo, creatureX?: number, creatureY?: number, creatureZ?: number): number | undefined;
