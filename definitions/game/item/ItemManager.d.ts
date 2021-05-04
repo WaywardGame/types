dfd71d99fd34b07ea9a53c490d6b4678a79ca5a8
Gary Wilber <spacetech326@gmail.com>
2021-05-04 10:24:35 -0700

    Update for 2.10.3-beta.dev.20210504.2

--- definitions/game/item/ItemManager.d.ts
+++ definitions/game/item/ItemManager.d.ts
@@ -115,7 +115,7 @@ export default class ItemManager extends EventEmitter.Host<IItemManagerEvents> {
     spawn(terrainType: TerrainType, x: number, y: number, z: number): void;
     getTileContainer(x: number, y: number, z: number, tile?: ITile): IContainer;
     getRandomQuality(bonusQuality?: number): Quality;
-    hasAdditionalRequirements(actionOrHuman: IActionHandlerApi<Player | NPC> | Human, craftType: ItemType, message?: Message, useDoodad?: boolean, isRepairOrDisassembly?: boolean): IRequirementInfo;
+    hasAdditionalRequirements(actionOrHuman: IActionHandlerApi<Player | NPC> | Human, craftType: ItemType, message?: Message, faceDoodad?: boolean, isRepairOrDisassembly?: boolean): IRequirementInfo;
     getItemTypeGroupName(itemType: ItemType | ItemTypeGroup, article?: boolean, count?: number): Translation;
     isInGroup(itemType: ItemType, itemGroup: ItemTypeGroup | ItemType): boolean;
     craft(action: IActionHandlerApi<Player | NPC>, itemType: ItemType, itemsToRequire: Item[], itemsToConsume: Item[], baseItem?: Item): CraftStatus;
