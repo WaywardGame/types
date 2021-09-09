49ffcc8dc774faaeb8b6166e35f87e1b706db6a2
Gary Wilber <spacetech326@gmail.com>
2021-09-09 02:43:32 -0700

    Update for 2.10.8-beta.dev.20210909.1

--- definitions/multiplayer/Multiplayer.d.ts
+++ definitions/multiplayer/Multiplayer.d.ts
@@ -78,7 +78,7 @@ export default class Multiplayer extends EventEmitter.Host<IMultiplayerEvents> {
     getDedicatedServerMatchmakingInfo(matchmakingServer: string, identifier?: string): IMatchmakingInfo;
     getBannedPlayers(): string[];
     setBanned(identifier: string, ban: boolean): boolean;
-    createServer(serverInfo: ServerInfo, options?: IMultiplayerOptions): void;
+    createServer(serverInfo: ServerInfo): void;
     joinServer(serverInfo: ServerInfo, options?: Partial<IJoinServerOptions>): void;
     disconnect(reason?: DisconnectReason, args?: any[], unloadingOrResetGameState?: boolean): Promise<void>;
     displayJoinServerRetryDialog(matchmakingInfo: IMatchmakingInfo, retryReason: JoinServerRetryReason): Promise<void>;
