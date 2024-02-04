db5d78671b536d61d56e830510bc96a001f3c5cb
Spacetech <spacetech326@gmail.com>
2024-02-04 04:05:08 -0800

    Update for 2.14.0-beta.dev.20240204.1

--- definitions/utilities/collection/Arrays.d.ts
+++ definitions/utilities/collection/Arrays.d.ts
@@ -13,20 +13,20 @@ declare namespace Arrays {
     const EMPTY: readonly [];
     function repeat<A extends any[]>(count: number, ...values: A): Array<A[number]>;
     /**
-     * Produces an array of the numbers @param min (inclusive) to @param max (exclusive), in steps of @param step
+     * Produces an array of the numbers `min` (inclusive) to `max` (exclusive), in steps of `step`
      * @param min A number, inclusive
      * @param max A number, exclusive
      * @param step The difference between each step between min and max, defaulting to 1
      */
     function range(min: number, max: number, step: number): number[];
     /**
-     * Produces an array of the numbers @param min (inclusive) to @param max (exclusive)
+     * Produces an array of the numbers `min` (inclusive) to `max` (exclusive)
      * @param min A number, inclusive
      * @param max A number, exclusive
      */
     function range(min: number, max: number): number[];
     /**
-     * Produces an array of the numbers 0 through @param max (exclusive)
+     * Produces an array of the numbers 0 through `max` (exclusive)
      * @param max A number, exclusive
      */
     function range(max: number): number[];
