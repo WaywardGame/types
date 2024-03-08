5b57f5b8663a196336a7a33e0b56f2de36d0a514
Spacetech <spacetech326@gmail.com>
2024-03-08 04:05:20 -0800

    Update for 2.14.0-beta.dev.20240308.1

--- definitions/game/game/tile/TerrainResources.d.ts
+++ definitions/game/game/tile/TerrainResources.d.ts
@@ -9,6 +9,7 @@
  * https://github.com/WaywardGame/types/wiki
  */
 import { BiomeType } from "@wayward/game/game/biome/IBiome";
+import { DoodadType } from "@wayward/game/game/doodad/IDoodad";
 import type { ILootItem } from "@wayward/game/game/ILoot";
 import { ItemType } from "@wayward/game/game/item/IItem";
 import { TerrainType } from "@wayward/game/game/tile/ITerrain";
@@ -20,6 +21,14 @@ export interface ITerrainLoot {
     items?: ITerrainLootItem[] | OptionalDescriptions<BiomeType, ITerrainLootItem[]>;
     defaultItem?: ItemType;
     disableWaterRouting?: boolean;
+    /**
+     * Set to a doodad type to allow gathering from the terrain as a doodad (in its ripening stage) like in the case of grass.
+     */
+    doodadGather?: DoodadType;
+    /**
+     * When set, the terrain will change to this type when the tile is gathered from as a doodad (via doodadGather).
+     */
+    doodadTileChange?: TerrainType;
 }
 export declare const resource: OptionalDescriptions<TerrainType, ITerrainLoot>;
 export default resource;
