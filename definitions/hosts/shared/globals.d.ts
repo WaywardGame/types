bf1cfe7ee450d96fad7fca1624d74a2aecb14536
Spacetech <824323+Spacetech@users.noreply.github.com>
2025-07-01 07:02:21 -0700

    Update for 2.14.6-beta.dev.20250701.1

--- definitions/hosts/shared/globals.d.ts
+++ definitions/hosts/shared/globals.d.ts
@@ -9,8 +9,8 @@
  * https://github.com/WaywardGame/types/wiki
  */
 import type { IWaywardTitle } from "@wayward/hosts/shared/globalTypes";
-export declare const gameVersionStage = "beta", gameVersionMajor = 2, gameVersionMinor = 14, gameVersionPatch = 5, gameVersionName = "Runekeeper", gameVersionColor = 11015763;
-export declare const gameVersion = "beta2.14.5";
+export declare const gameVersionStage = "beta", gameVersionMajor = 2, gameVersionMinor = 14, gameVersionPatch = 6, gameVersionName = "Runekeeper", gameVersionColor = 11015763;
+export declare const gameVersion = "beta2.14.6";
 export declare const gameVersionTitleMajor = "Wayward: Runekeeper";
 export declare const gameVersionTitleMinor: string;
 export declare const gameVersionTitle: string;
