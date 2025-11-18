407505b9abb23d9ae682e48f8a4f0e161b5cdb03
Spacetech <824323+Spacetech@users.noreply.github.com>
2025-11-18 06:02:08 -0800

    Update for 2.15.2-beta.dev.20251118.1

--- definitions/game/game/entity/action/actions/RemovePlayer.d.ts
+++ definitions/game/game/entity/action/actions/RemovePlayer.d.ts
@@ -10,5 +10,5 @@
  */
 import { Action } from "@wayward/game/game/entity/action/Action";
 import { ActionArgument, ActionType } from "@wayward/game/game/entity/action/IAction";
-declare const _default: Action<[ActionArgument.Player], ActionType.RemovePlayer, import("../../Human").default<unknown, number, import("../../../reference/IReferenceManager").ReferenceType.NPC | import("../../../reference/IReferenceManager").ReferenceType.Player, unknown>, void, import("@wayward/game/game/entity/action/IAction").IActionUsable, [import("../../player/Player").default]>;
+declare const _default: Action<[ActionArgument.String], ActionType.RemovePlayer, import("../../Human").default<unknown, number, import("../../../reference/IReferenceManager").ReferenceType.NPC | import("../../../reference/IReferenceManager").ReferenceType.Player, unknown>, void, import("@wayward/game/game/entity/action/IAction").IActionUsable, [string]>;
 export default _default;
