2a5858d72b18c426ef3922eee52b0941c4fcb23c
Spacetech <824323+Spacetech@users.noreply.github.com>
2026-02-10 06:07:38 -0800

    Update for 2.15.3-beta.dev.20260210.1

--- definitions/game/game/entity/action/actions/Learn.d.ts
+++ definitions/game/game/entity/action/actions/Learn.d.ts
@@ -11,7 +11,7 @@
 import { Action } from "@wayward/game/game/entity/action/Action";
 import { ActionArgument, ActionType } from "@wayward/game/game/entity/action/IAction";
 import { ItemTypeGroup, RecipeLevel } from "@wayward/game/game/item/IItem";
-export declare const ACTION_LEARN_RECIPE_OF_ISLAND_MULTIPLIER = 8;
+export declare const ACTION_LEARN_RECIPE_OF_ISLAND_MULTIPLIER = 15;
 export declare const ACTION_LEARN_RECIPE_WORTH_PENALTY_MULTIPLIER = 2;
 export declare const ACTION_LEARN_RECIPE_LEVEL_WEIGHTS: Record<RecipeLevel, number>;
 export declare const ACTION_LEARN_RECIPE_ITEM_GROUP_WEIGHTS: PartialRecord<ItemTypeGroup, number>;
